96ae5f0155041408830632f6275cbabfaf3f7521
michaelOrSd <michael.oreilly@keyin.com>
2024-03-05 13:12:04 -0330

    margin changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = () => {
         src="https://pocket-homes.s3.us-east-2.amazonaws.com/assets/HeroImage.webp" 
         alt="Tiny Home Village" 
       />
-      <div className='bg-black/30 absolute top-0 left-0 w-full h-screen'/>
+      <div className='bg-black/50 absolute top-0 left-0 w-full h-screen'/>
       <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
         <div className='md:left-[10%] max-w-[1200px] m-auto absolute p-4'>
-            <p>Pocket Home Communities</p>
-            <h1 className='font-bold text-5xl md:-7xl drop-shadow-2xl'>Where Home Meets Harmony</h1>
-            <p className='max-w-[600px] drop-shadow-2xl py-2 text-xl'>
+            <p className='pb-0 text-lg'>Pocket Home Communities</p>
+            <h1 className='pb-0 font-bold text-5xl md:-7xl drop-shadow-2xl'>Where Home Meets Harmony</h1>
+            <p className='max-w-[600px] drop-shadow-2xl py-2 pb-4 text-xl'>
             Embrace sustainable, connected, and affordable living in our pocket home community. Together, let's build a future that cherishes our planet, fosters community, and offers everyone a place to call home.
             </p>
             <button className='bg-white text-black'>Sign Up Now</button>
--- src/components/Vaules.jsx
+++ src/components/Vaules.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Values = () => {
   return (
-    <div className='max-w-[900px] m-auto px-4 py-12 flex flex-wrap justify-between'>
-        <p className='text-2xl font-bold text-gray-700'>Community</p>
-        <p className='text-2xl font-bold text-gray-700'>Sustainability</p>
-        <p className='text-2xl font-bold text-gray-700'>Affordability</p>
+    <div className='max-w-[900px] m-auto px-4 pt-16 flex flex-wrap justify-between'>
+        <p className='p-4 text-4xl font-bold text-gray-700'>Community</p>
+        <p className='p-4 text-4xl font-bold text-gray-700'>Sustainability</p>
+        <p className='p-4 text-4xl font-bold text-gray-700'>Affordability</p>
     </div>
   )
 }
